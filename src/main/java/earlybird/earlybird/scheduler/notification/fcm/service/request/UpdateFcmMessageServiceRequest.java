b094bcfd5d4e66a0bc117eab9a851d2b4701c90d
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-16 01:57:04 +0900

    [fix] getter 수정

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/UpdateFcmMessageServiceRequest.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/UpdateFcmMessageServiceRequest.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Builder
 @Getter
@@ -12,4 +14,8 @@ public class UpdateFcmMessageServiceRequest {
     private Long notificationId;
     private String deviceToken;
     private LocalDateTime targetTime;
+
+    public Instant getTargetTimeInstant() {
+        return targetTime.atZone(ZoneId.of("Asia/Seoul")).toInstant();
+    }
 }
