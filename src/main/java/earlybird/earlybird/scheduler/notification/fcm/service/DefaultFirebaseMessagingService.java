c34259ca8bb08d8b7a8256c7a68dd6444c1db435
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-16 19:27:21 +0900

    [fix] 필요없는 fcm 설정 삭제

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/DefaultFirebaseMessagingService.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/DefaultFirebaseMessagingService.java
@@ -15,13 +15,6 @@ public class DefaultFirebaseMessagingService implements FirebaseMessagingService
                         .setBody(request.getBody())
                         .build())
                 .setToken(request.getDeviceToken())
-                .setAndroidConfig(AndroidConfig.builder()
-                        .setPriority(HIGH)
-                        .build())
-                .setApnsConfig(ApnsConfig.builder()
-                        // TODO: 만약 실패하면 값을 5로 변경해보기
-                        .putHeader("apns-priority", "10")
-                        .build())
                 .build());
     }
 }
