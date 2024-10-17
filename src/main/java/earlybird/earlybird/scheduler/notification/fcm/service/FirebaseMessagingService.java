b733ce1230e3bb522c1549e35004093811ec9c06
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-17 23:09:01 +0900

    [refactor] 불필요한 주석 삭제

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/FirebaseMessagingService.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/FirebaseMessagingService.java
@@ -3,10 +3,6 @@ package earlybird.earlybird.scheduler.notification.fcm.service;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import earlybird.earlybird.scheduler.notification.fcm.service.request.SendMessageByTokenServiceRequest;
 
-/**
- * 테스트 코드 작성 시 firebase messaging 관련 mocking 을 위해 인터페이스를 정의함
- * -> 테스트코드에서 DI로 주입
- */
 public interface FirebaseMessagingService {
     String send(SendMessageByTokenServiceRequest request) throws FirebaseMessagingException;
 }
