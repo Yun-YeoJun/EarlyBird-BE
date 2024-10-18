9de1d86dbf25ed335c08527ec9dffac554df2601
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-18 20:10:33 +0900

    [refactor] FcmNotificationStatus -> NotificationStatus로 이름 변경 & MODIFIED 상태 추가

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/domain/NotificationStatus.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/domain/NotificationStatus.java
@@ -5,10 +5,11 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum FcmNotificationStatus {
+public enum NotificationStatus {
 
     PENDING("전송 대기 중"),
     COMPLETED("전송 완료"),
+    MODIFIED("변경됨"),
     CANCELLED("취소");
 
     private final String text;
