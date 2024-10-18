caf57a685c63db662f3565ea755c043fe1951f0a
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-18 20:12:42 +0900

    [refactor] 불필요한 코드 삭제

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/response/RegisterFcmMessageAtSchedulerServiceResponse.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/response/RegisterFcmMessageAtSchedulerServiceResponse.java
@@ -2,11 +2,9 @@ package earlybird.earlybird.scheduler.notification.fcm.service.response;
 
 import earlybird.earlybird.appointment.domain.Appointment;
 import earlybird.earlybird.scheduler.notification.fcm.domain.FcmNotification;
-import earlybird.earlybird.scheduler.notification.fcm.domain.FcmNotificationStatus;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
