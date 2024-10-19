2be5cb19916793672d4a764c1363ceec9870217b
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-10-19 20:50:23 +0900

    [feature] 스프링의 Retry를 활용하여 FCM API 호출 실패 시 최대 5번 재호출하는 기능 구현

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/FirebaseMessagingService.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/FirebaseMessagingService.java
@@ -2,7 +2,10 @@ package earlybird.earlybird.scheduler.notification.fcm.service;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
 import earlybird.earlybird.scheduler.notification.fcm.service.request.SendMessageByTokenServiceRequest;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 public interface FirebaseMessagingService {
+    @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 1000))
     String send(SendMessageByTokenServiceRequest request) throws FirebaseMessagingException;
 }
