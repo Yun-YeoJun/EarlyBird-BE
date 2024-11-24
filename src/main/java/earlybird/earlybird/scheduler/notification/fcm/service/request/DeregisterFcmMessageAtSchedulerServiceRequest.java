0c321dc54fcc74e6b4009cb579f17e6fc410d9fb
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-11-25 00:17:46 +0900

    [feature] DTO 변환 로직 추가

--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/DeregisterFcmMessageAtSchedulerServiceRequest.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/DeregisterFcmMessageAtSchedulerServiceRequest.java
@@ -1,8 +1,13 @@
 package earlybird.earlybird.scheduler.notification.fcm.service.request;
 
+import earlybird.earlybird.appointment.domain.AppointmentUpdateType;
+import earlybird.earlybird.appointment.service.request.UpdateAppointmentServiceRequest;
 import earlybird.earlybird.scheduler.notification.fcm.domain.NotificationStatus;
+import earlybird.earlybird.scheduler.notification.fcm.domain.NotificationUpdateType;
 import lombok.Builder;
 import lombok.Getter;
+
+import static earlybird.earlybird.appointment.domain.AppointmentUpdateType.*;
 
 @Builder
 @Getter
@@ -10,4 +15,19 @@ public class DeregisterFcmMessageAtSchedulerServiceRequest {
     private String clientId;
     private Long appointmentId;
     private NotificationStatus targetNotificationStatus;
+
+    public static DeregisterFcmMessageAtSchedulerServiceRequest from(UpdateAppointmentServiceRequest request) {
+        AppointmentUpdateType updateType = request.getUpdateType();
+        NotificationStatus targetStatus = switch (updateType) {
+            case POSTPONE -> NotificationStatus.POSTPONE;
+            case MODIFY -> NotificationStatus.MODIFIED;
+        };
+
+        return DeregisterFcmMessageAtSchedulerServiceRequest.builder()
+                .appointmentId(request.getAppointmentId())
+                .clientId(request.getClientId())
+                .targetNotificationStatus(targetStatus)
+                .build();
+
+    }
 }
--- src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest.java
+++ src/main/java/earlybird/earlybird/scheduler/notification/fcm/service/request/RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest.java
@@ -1,12 +1,14 @@
 package earlybird.earlybird.scheduler.notification.fcm.service.request;
 
 import earlybird.earlybird.appointment.domain.Appointment;
+import earlybird.earlybird.appointment.service.request.CreateAppointmentServiceRequest;
+import earlybird.earlybird.appointment.service.request.UpdateAppointmentServiceRequest;
+import earlybird.earlybird.common.LocalDateTimeUtil;
+import earlybird.earlybird.common.LocalDateUtil;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.*;
 
 @Getter
 public class RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest {
@@ -59,6 +61,58 @@ public class RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest {
                 .preparationTime(request.getPreparationTime())
                 .movingTime(request.getMovingTime())
                 .appointmentTime(request.getAppointmentTime())
+                .build();
+    }
+
+    public static RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest from(Appointment appointment) {
+
+        LocalDateTime appointmentTime = appointment.getAppointmentTime().atDate(LocalDateUtil.getLocalDateNow().plusDays(2));
+        LocalDateTime movingTime = LocalDateTimeUtil.subtractDuration(appointmentTime, appointment.getMovingDuration());
+        LocalDateTime preparationTime = LocalDateTimeUtil.subtractDuration(movingTime, appointment.getPreparationDuration());
+
+        return RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest.builder()
+                .clientId(appointment.getClientId())
+                .deviceToken(appointment.getDeviceToken())
+                .appointment(appointment)
+                .preparationTime(preparationTime)
+                .movingTime(movingTime)
+                .appointmentTime(appointmentTime)
+                .build();
+    }
+
+    public static RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest from(
+            CreateAppointmentServiceRequest request, Appointment appointment
+    ) {
+
+        LocalDateTime firstAppointmentTime = request.getFirstAppointmentTime();
+        LocalDateTime movingTime = LocalDateTimeUtil.subtractDuration(firstAppointmentTime, request.getMovingDuration());
+        LocalDateTime preparationTime = LocalDateTimeUtil.subtractDuration(movingTime, request.getPreparationDuration());
+
+        return RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest.builder()
+                .clientId(request.getClientId())
+                .deviceToken(request.getDeviceToken())
+                .appointmentTime(firstAppointmentTime)
+                .preparationTime(preparationTime)
+                .movingTime(movingTime)
+                .appointment(appointment)
+                .build();
+    }
+
+    public static RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest from(
+            UpdateAppointmentServiceRequest request, Appointment appointment
+    ) {
+
+        LocalDateTime firstAppointmentTime = request.getFirstAppointmentTime();
+        LocalDateTime movingTime = LocalDateTimeUtil.subtractDuration(firstAppointmentTime, request.getMovingDuration());
+        LocalDateTime preparationTime = LocalDateTimeUtil.subtractDuration(movingTime, request.getPreparationDuration());
+
+        return RegisterFcmMessageForExistingAppointmentAtSchedulerServiceRequest.builder()
+                .clientId(request.getClientId())
+                .deviceToken(request.getDeviceToken())
+                .appointment(appointment)
+                .preparationTime(preparationTime)
+                .movingTime(movingTime)
+                .appointmentTime(firstAppointmentTime)
                 .build();
     }
 }
