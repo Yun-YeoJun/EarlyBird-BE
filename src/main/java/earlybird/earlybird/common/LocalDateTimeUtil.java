76eda0d31f10754c6c292704547402c5fdcba874
윤여준 <30434779+Yun-YeoJun@users.noreply.github.com>
2024-11-25 00:12:48 +0900

    [feature] Util 기능 구현

--- src/main/java/earlybird/earlybird/common/LocalDateTimeUtil.java
+++ src/main/java/earlybird/earlybird/common/LocalDateTimeUtil.java
@@ -1,10 +1,18 @@
 package earlybird.earlybird.common;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class LocalDateTimeUtil {
     public static LocalDateTime getLocalDateTimeNow() {
         return LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
+
+    public static LocalDateTime subtractDuration(LocalDateTime localDateTime, Duration duration) {
+        if (localDateTime == null || duration == null) {
+            throw new IllegalArgumentException("localDateTime and duration can't be null");
+        }
+        return localDateTime.minus(duration);
     }
 }
---
+++ src/main/java/earlybird/earlybird/common/LocalDateUtil.java
@@ -0,0 +1,10 @@
+package earlybird.earlybird.common;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class LocalDateUtil {
+    public static LocalDate getLocalDateNow() {
+        return LocalDate.now(ZoneId.of("Asia/Seoul"));
+    }
+}
